08ae35e84f601e50c9798c2edd95cd4e7984f250
HaMM4R <HaMM4RTime@hotmail.com>
2020-10-06 17:15:43 +0100

    Audio Visualiser and basic light sync

--- Platformer/Src/Levels/PulseLight.gd
+++ Platformer/Src/Levels/PulseLight.gd
@@ -2,29 +2,43 @@ extends Light2D
 
 export var pulseDuration: = 1.0
 onready var light: = get_node(".")
+onready var audioVisualiser = get_node("AudioTest/visualiser")
+
 var increasing: = false
 var pulseReduce: = false
 
 func _ready():
 	light.energy = 0.0
-	_pulse()
+
 
 func _physics_process(delta):
-	if !increasing:
-		return
-
-	if light.energy <= 1 and !pulseReduce:
-		light.energy += 0.2
-	else:
-		pulseReduce = true
-		light.energy -= 0.2
+	var total = 0
+	var mean = 0 
 	
-	if light.energy <= 0 and pulseReduce:
-		pulseReduce = false
-		_pulse()
+	for i in range(70):
+		total += audioVisualiser.histogram[i]
+		
+	mean = total / 70
+	light.energy = mean * 8
 	
-func _pulse():
-	increasing = false
-	yield(get_tree().create_timer(pulseDuration), "timeout")
-	increasing = true
+#	if !increasing:
+#		return
+#
+#	if light.energy <= 1 and !pulseReduce:
+#		light.energy += 0.2
+#	else:
+#		pulseReduce = true
+#		light.energy -= 0.2
+#
+#	if light.energy <= 0 and pulseReduce:
+#		pulseReduce = false
 
+	
+#func _pulse():
+#	increasing = false
+#	yield(get_tree().create_timer(pulseDuration), "timeout")
+#	increasing = true
+
+func _test():
+	yield(get_tree().create_timer(0.4), "timeout")
+	light.energy = 0
